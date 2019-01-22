a826d7cd1874666d55d9f442fb251cf98bf37a50
ENvironmentSet <sbshw1@gmail.com>
2019-01-22 19:16:01 +0900

    Fix typo in index export file of src directory

--- packages/operators/src/index.js
+++ packages/operators/src/index.js
@@ -3,4 +3,4 @@ export { default as reduce } from './reduce';
 export { default as flatMap } from './flatMap';
 export { default as toInputOperator } from './toInputOperator';
 export { default as toOutputOperator } from './toOutputOperator';
-export { default as toEchshpere } from './toEcoshpere';
+export { default as toEcoshpere } from './toEcoshpere';
